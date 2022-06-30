9cc7801a3ccaafe5fc0dc020e6bf83430e52d2e0
Antony Maziero Warken <againplayi7@gmail.com>
2022-06-30 15:01:56 -0300

    cap 3 vid 1 finalizado

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -34,4 +34,20 @@ roteador.delete('/:id', async (requisicao, resposta) => {
   resposta.end()
 })
 
+roteador.get('/:id', async (requisicao, resposta, proximo) => {
+  try {
+    const dados = {
+      id: requisicao.params.id,
+      idFornecedor: requisicao.fornecedor.id
+    }
+    const produto = new Produto(dados)
+    await produto.carregar()
+    resposta.send(
+      JSON.stringify(produto)
+    )
+  } catch(erro) {
+    proximo(erro)
+  }
+})
+
 module.exports = roteador
