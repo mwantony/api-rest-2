8278b5a5c8868b8e78a3512ae378a3d591bd0cb2
Antony Maziero Warken <againplayi7@gmail.com>
2022-06-30 15:17:29 -0300

    cap 3 vid 2 finalizado

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,10 +1,14 @@
 const roteador = require('express').Router({mergeParams: true})
 const Tabela = require('./TabelaProduto')
 const Produto = require('./Produto')
+const Serializador = require('../../../Serializador').SerializadorProduto
 roteador.get('/', async (requisicao, resposta) => {
   const produtos = await Tabela.listar(requisicao.fornecedor.id)
+  const serializador = new Serializador(
+    resposta.getHeader('Content-Type')
+  )
   resposta.send(
-    JSON.stringify(produtos)
+    serializador.serializar(produtos)
   )
 })
 
@@ -15,8 +19,13 @@ roteador.post('/', async (requisicao, resposta, proximo) => {
     const dados = Object.assign({}, corpo, { fornecedor: idFornecedor })
     const produto = new Produto(dados)
     await produto.criar()
+    const serializador = new Serializador(
+      resposta.getHeader('Content-Type')
+    )
     resposta.status(201)
-    resposta.send(produto)
+    resposta.send(
+      serializador.serializar(produto)
+    )
     resposta.end()
   } catch(erro) {
     proximo(erro)
@@ -42,8 +51,12 @@ roteador.get('/:id', async (requisicao, resposta, proximo) => {
     }
     const produto = new Produto(dados)
     await produto.carregar()
+    const serializador = new Serializador(
+      resposta.getHeader('Content-Type'),
+      ['preco', 'estoque', 'fornecedor', 'dataCriacao', 'dataAtualizacao', 'versao']
+    )
     resposta.send(
-      JSON.stringify(produto)
+      serializador.serializar(produto)
     )
   } catch(erro) {
     proximo(erro)
